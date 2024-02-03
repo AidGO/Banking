7992fa6da4e8de60447a50f61bb1c4ea583e23b5
AidGO <ayolsen14@gmail.com>
2024-02-03 01:18:16 -0600

    first commit

--- src/views/footerview.php
+++ src/views/footerview.php
@@ -1,6 +1,6 @@
 <footer>
     Github link<br>
-    <a href="https://github.com/AidGO/HomePage">https://github.com/AidGO/HomePage</a>
+    <a href="https://github.com/AidGO/HomePage">https://github.com/AidGO/Banking</a>
 </footer>
 </body>
 </html>
